6cc464abdc2f7e69119b3c213da1f8fd13a7d01d
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2020-05-11 21:08:17 -0400

    mini-project-js

--- script.js
+++ script.js
@@ -3,26 +3,23 @@ const confirmPassword = document.querySelector('#confirmPassword');
 const showError = document.querySelector('.error-message')
 const submit = document.querySelector('#btnSubmit');
 const clear = document.querySelector('#clear');
+const form = document.querySelector('#form');
 
-let passwordMatch = document.createElement('p');
-    passwordMatch.innerText = "Your passwords didn't match! Please, provide the same password in each field"
-    passwordMatch.style.background = 'lightpink';
-
-let lengthPassword = document.createElement('p');
-    lengthPassword.innerText = "Your passwords is too short! Please, provide a password that is at least 10 characters long."
-    lengthPassword.style.background = 'lightpink';
+let lengthPassword = "Your passwords is too short! Please, provide a password that is at least 10 characters long."
+let passwordMatch = "Your passwords didn't match! Please, provide the same password in each field";
 
 function validateForm(event){
+    removeAllErrors();
     event.preventDefault();
     const termsChecked = document.querySelector('#terms').checked;
     if(password.value.length < 10) {
         password.style.border = '1px solid red';
-        password.addEventListener('focus', validateForm);
-        showError.appendChild(lengthPassword);
+        password.focus();
+        addError(lengthPassword);
     } else if(password.value !== confirmPassword.value) {
         confirmPassword.style.border = '1px solid red';
         confirmPassword.focus();
-        showError.appendChild(passwordMatch);
+        addError(passwordMatch);
     } else if (!termsChecked){  
         alert("You must agree with our terms.");
     } else {
@@ -33,11 +30,21 @@ function validateForm(event){
     }
 }
 
-function clean(){
-    let form = document.getElementById('form');
-        form.submit();
+form.addEventListener('submit', validateForm);
+
+function addError(message){
+    let errorMessage = document.createElement('p');
+        errorMessage.innerText = message;
+        errorMessage.style.background = 'lightpink';
+    // add error to DOM
+        showError.appendChild(errorMessage);
+    // give element classname like .error-message
 }
 
-submit.addEventListener('click', validateForm);
-clear.addEventListener('click', clean);
-
+function removeAllErrors() {
+  // empty `showError` container
+    showError.innerText = '';
+  // Reset border in password and confirm password inputs
+    confirmPassword.style.border = '1px solid lightgray';
+    password.style.border = '1px solid lightgray';
+}
